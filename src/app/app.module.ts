c1ee140e3712d4bded4df1b58c3b408ccd23723d
Mark Boon <mmarkk1994@gmail.com>
2019-11-26 14:20:27 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { RegisterComponent } from './register/register.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { LoginComponent } from './login/login.component';
+import { GuestComponent } from './guest/guest.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClientComponent } from './client/client.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { AdminComponent } from './admin/admin.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    StatisticsComponent,
+    RegisterComponent,
+    ProjectsComponent,
+    NotificationsComponent,
+    LoginComponent,
+    GuestComponent,
+    FooterComponent,
+    ClientComponent,
+    TeacherComponent,
+    AdminComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
