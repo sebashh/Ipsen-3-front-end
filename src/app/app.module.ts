cef0e4f95096b4aad0f631289404db71da8afbdc
Vincent <vincent.nuis@Gmail.com>
2019-11-26 14:58:36 +0100

    Fixed bug
    removed some imports/declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { TopbarComponent } from './topbar/topbar.component';
-import { StatisticsComponent } from './statistics/statistics.component';
-import { RegisterComponent } from './register/register.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { LoginComponent } from './login/login.component';
-import { GuestComponent } from './guest/guest.component';
-import { FooterComponent } from './footer/footer.component';
-import { ClientComponent } from './client/client.component';
-import { TeacherComponent } from './teacher/teacher.component';
-import { AdminComponent } from './admin/admin.component';
-import { StudentComponent } from './student/student.component';
 import { ModelComponent } from './model/model.component';
 import { ControllerComponent } from './controller/controller.component';
 import { ViewComponent } from './view/view.component';
 import { ServicesComponent } from './services/services.component';
-import { TemplateComponent } from './template/template.component';
 import { TemplatesComponent } from './templates/templates.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent,
-    StatisticsComponent,
-    RegisterComponent,
-    ProjectsComponent,
-    NotificationsComponent,
-    LoginComponent,
-    GuestComponent,
-    FooterComponent,
-    ClientComponent,
-    TeacherComponent,
-    AdminComponent,
-    StudentComponent,
     ModelComponent,
     ControllerComponent,
     ViewComponent,
     ServicesComponent,
-    TemplateComponent,
     TemplatesComponent
   ],
   imports: [
