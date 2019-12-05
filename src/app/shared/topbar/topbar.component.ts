a7364a993cc0b422241129389e328f087a5dec7c
Vincent <vincent.nuis@Gmail.com>
2019-12-05 15:12:04 +0100

    Login and Logout text clickable

--- src/app/shared/topbar/topbar.component.ts
+++ src/app/shared/topbar/topbar.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
+  isUserLoggedIn = false;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  message(){
+    console.log('Hello');
+  }
+
+  logIn(){
+    this.isUserLoggedIn = true;
+  }
+
+  logOut(){
+    this.isUserLoggedIn = false;
+  }
+
 
 }
