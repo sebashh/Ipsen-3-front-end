51e70646621bf32aa9fb49b55270cec6855b9b56
Yoran de Vos <yorandevos12@gmail.com>
2020-01-29 13:58:56 +0100

    register feedback

--- src/app/user/register/register-student/register-student.component.ts
+++ src/app/user/register/register-student/register-student.component.ts
@@ -6,6 +6,7 @@ import {RestApiService} from "../../../shared/Services/api-service";
 import {Category} from "../../../shared/Models/category.model";
 import {StudyService} from "../../../shared/Services/study.service";
 import {Study} from "../../../shared/Models/study.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register-student',
@@ -24,7 +25,8 @@ export class RegisterStudentComponent implements OnInit {
   constructor(formBuilder: FormBuilder,
               public restApiService: RestApiService,
               public categoryService: CategoryService,
-              public studyService: StudyService
+              public studyService: StudyService,
+              private router: Router
   ) {
 
     this.email = formBuilder.group({
@@ -62,7 +64,15 @@ export class RegisterStudentComponent implements OnInit {
         alert('You can only submit one study');
       } else {
         this.student = new Student(this.selectedStudies[0].id, this.getCategoryIdList(), this.email.get('currentEmail').value, this.password.get('currentPassword').value);
-        this.restApiService.registerUser('users/student', this.student);
+
+        this.restApiService.registerUser('users/student', this.student).subscribe((data) => {
+          if(data) {
+            window.alert('register successful!');
+            this.router.navigateByUrl('/home');
+          }
+        });
+
+
       }
     }
   }
